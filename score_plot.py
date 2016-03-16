91c096845087593075672418bbf0016ce130daba
c2tao <b97901182@gmail.com>
2016-03-17 01:46:52 +0800

    messy plots

--- score_plot.py
+++ score_plot.py
@@ -1,160 +1,300 @@
-from lookup import *
-
 import sys
 import cPickle as pickle
 import numpy as np 
-eng_short = filter(lambda x: x.split('_')[0]=='eng' and x.split('_')[2]=='0',qer_list)[:10]
-xit_short = filter(lambda x: x.split('_')[0]=='xit' and x.split('_')[2]=='0',qer_list)[:10]
+info_text ='''\
+eng iter1_MR0_50_3 0.170633116433
+eng iter1_MR1_50_3 0.152116919102
+eng iter1_MR2_50_3 0.12000586423
+eng iter2_MR0_50_3 0.164116619998
+eng iter2_MR1_50_3 0.10503527382
+eng iter1_MR0_50_5 0.054312992579
+eng iter1_MR1_50_5 0.0829084718971
+eng iter1_MR2_50_5 0.133466575616
+eng iter2_MR0_50_5 0.0900616122627
+eng iter2_MR1_50_5 0.136131220296
+eng iter1_MR0_50_7 0.0495592962732
+eng iter1_MR1_50_7 0.0938252007345
+eng iter1_MR2_50_7 0.0490411688964
+eng iter2_MR0_50_7 0.0750231649676
+eng iter2_MR1_50_7 0.0609959903765
+eng iter1_MR0_100_3 0.0254822706186
+eng iter1_MR1_100_3 0.0861607472805
+eng iter1_MR2_100_3 0.0622789484868
+eng iter2_MR0_100_3 0.0794854491743
+eng iter2_MR1_100_3 0.114957801917
+eng iter1_MR0_100_5 0.0962664933066
+eng iter1_MR1_100_5 0.0776571780541
+eng iter1_MR2_100_5 0.0867933052339
+eng iter2_MR0_100_5 0.0675394101412
+eng iter2_MR1_100_5 0.109441433306
+eng iter1_MR0_100_7 0.043678108704
+eng iter1_MR1_100_7 0.0249799616706
+eng iter1_MR2_100_7 0.0514606720371
+eng iter2_MR0_100_7 0.0686727794997
+eng iter2_MR1_100_7 0.0739535145325
+eng iter1_MR0_300_3 0.0933020744468
+eng iter1_MR1_300_3 0.143279846536
+eng iter1_MR2_300_3 0.121198042284
+eng iter2_MR0_300_3 0.0760960195255
+eng iter2_MR1_300_3 0.149551235527
+eng iter1_MR0_300_5 0.0681518713905
+eng iter1_MR1_300_5 0.0566089299779
+eng iter1_MR2_300_5 0.0839699134904
+eng iter2_MR0_300_5 0.0735000261997
+eng iter2_MR1_300_5 0.0920983204876
+eng iter1_MR0_300_7 0.0783425357348
+eng iter1_MR1_300_7 0.0525749276056
+eng iter1_MR2_300_7 0.083645449631
+eng iter2_MR0_300_7 0.0666609551922
+eng iter2_MR1_300_7 0.103232950482
+eng iter1_MR0_500_3 0.13918117878
+eng iter1_MR1_500_3 0.0914791159133
+eng iter1_MR2_500_3 0.0674188633807
+eng iter2_MR0_500_3 0.133230999127
+eng iter2_MR1_500_3 0.121734676981
+eng iter1_MR0_500_5 0.067922528364
+eng iter1_MR1_500_5 0.0714125623299
+eng iter1_MR2_500_5 0.0873610361358
+eng iter2_MR0_500_5 0.127571064537
+eng iter2_MR1_500_5 0.106004964692
+eng iter1_MR0_500_7 0.0468270040387
+eng iter1_MR1_500_7 0.0793139413772
+eng iter1_MR2_500_7 0.0649214813446
+eng iter2_MR0_500_7 0.0698067670884
+eng iter2_MR1_500_7 0.102053795034
+xit iter1_MR0_50_3 0.269909010724
+xit iter1_MR1_50_3 0.268666357159
+xit iter1_MR2_50_3 0.327958257586
+xit iter2_MR0_50_3 0.207363211337
+xit iter2_MR1_50_3 0.223023392139
+xit iter1_MR0_50_5 0.221151182804
+xit iter1_MR1_50_5 0.0792162582092
+xit iter1_MR2_50_5 0.298717130205
+xit iter2_MR0_50_5 0.274857814725
+xit iter2_MR1_50_5 0.23199176681
+xit iter1_MR0_50_7 0.203304853639
+xit iter1_MR1_50_7 0.0745003300731
+xit iter1_MR2_50_7 0.154908075735
+xit iter2_MR0_50_7 0.188027927693
+xit iter2_MR1_50_7 0.281020068782
+xit iter1_MR0_50_9 0.163147532426
+xit iter1_MR1_50_9 0.0416968295548
+xit iter1_MR2_50_9 0.0759200096885
+xit iter2_MR0_50_9 0.153692501834
+xit iter2_MR1_50_9 0.056582990854
+xit iter1_MR0_100_3 0.149705806285
+xit iter1_MR1_100_3 0.232944172179
+xit iter1_MR2_100_3 0.230128014864
+xit iter2_MR0_100_3 0.256253204418
+xit iter2_MR1_100_3 0.171480563396
+xit iter1_MR0_100_5 0.176898828592
+xit iter1_MR1_100_5 0.14987472754
+xit iter1_MR2_100_5 0.178236085378
+xit iter2_MR0_100_5 0.211810733543
+xit iter2_MR1_100_5 0.251976185918
+xit iter1_MR0_100_7 0.126322733848
+xit iter1_MR1_100_7 0.141445951712
+xit iter1_MR2_100_7 0.147603920582
+xit iter2_MR0_100_7 0.221175118826
+xit iter2_MR1_100_7 0.219496082451
+xit iter1_MR0_100_9 0.0494478019187
+xit iter1_MR1_100_9 0.159145885235
+xit iter1_MR2_100_9 0.0998523387163
+xit iter2_MR0_100_9 0.247039498071
+xit iter2_MR1_100_9 0.114114478397
+xit iter1_MR0_300_3 0.208314910695
+xit iter1_MR1_300_3 0.13491716772
+xit iter1_MR2_300_3 0.219550493119
+xit iter2_MR0_300_3 0.271758402366
+xit iter2_MR1_300_3 0.255184779896
+xit iter1_MR0_300_5 0.126951716098
+xit iter1_MR1_300_5 0.251529940595
+xit iter1_MR2_300_5 0.186652419256
+xit iter2_MR0_300_5 0.154893536819
+xit iter2_MR1_300_5 0.235453932807
+xit iter1_MR0_300_7 0.162470086328
+xit iter1_MR1_300_7 0.14303355673
+xit iter1_MR2_300_7 0.144679258941
+xit iter2_MR0_300_7 0.32645085534
+xit iter2_MR1_300_7 0.221082181974
+xit iter1_MR0_300_9 0.100955829275
+xit iter1_MR1_300_9 0.0942386181608
+xit iter1_MR2_300_9 0.2142396903
+xit iter2_MR0_300_9 0.223305268088
+xit iter2_MR1_300_9 0.134300155487
+xit iter1_MR0_500_3 0.227659252084
+xit iter1_MR1_500_3 0.17692623741
+xit iter1_MR2_500_3 0.181100683008
+xit iter2_MR0_500_3 0.211606041157
+xit iter2_MR1_500_3 0.211482028662
+xit iter1_MR0_500_5 0.140840441316
+xit iter1_MR1_500_5 0.166469950045
+xit iter1_MR2_500_5 0.212952473982
+xit iter2_MR0_500_5 0.231451709361
+xit iter2_MR1_500_5 0.152080315496
+xit iter1_MR0_500_7 0.111824506443
+xit iter1_MR1_500_7 0.125499177106
+xit iter1_MR2_500_7 0.172832967736
+xit iter2_MR0_500_7 0.245161494544
+xit iter2_MR1_500_7 0.178349881931
+xit iter1_MR0_500_9 0.146695873937
+xit iter1_MR1_500_9 0.0557546955664
+xit iter1_MR2_500_9 0.131737559267
+xit iter2_MR0_500_9 0.0820275916252
+xit iter2_MR1_500_9 0.0659872700123
+eng mfc 0.11087828881
+eng mbf 0.133914986108
+xit mfc 0.0896568778182
+xit mbf 0.287111280203
+eng iter1_MR0 0.124996685276
+eng iter1_MR1 0.139851927412
+eng iter1_MR2 0.134255748975
+eng iter2_MR0 0.103793277746
+eng iter2_MR1 0.145180469421
+xit iter1_MR0 0.190067516728
+xit iter1_MR1 0.212798102375
+xit iter1_MR2 0.241793539842
+xit iter2_MR0 0.258187869961
+xit iter2_MR1 0.254435285465
+eng avg_itmr 0.152883329818
+xit avg_itmr 0.261781453266
+eng avg_all 0.180143987086
+xit avg_all 0.263335590608'''
 
-xqer_list = eng_short + xit_short
+def get_info():
+    info = {}
+    for line in info_text.split('\n'):
+        key = line.split()[0]+'_'+line.split()[1]
+        val = float(line.split()[2])
+        info[key] = val
+    return info
+info = get_info()
 
+t_list = ['iter1_MR0', 'iter1_MR1', 'iter1_MR2', 'iter2_MR0', 'iter2_MR1']
+l_list = ['eng', 'xit'] 
+m_list =  [3,5,7,9]#[3,5,7] for english
+n_list =  [50, 100, 300, 500]
 
-it_mr_list=['iter1_MR0','iter1_MR1','iter1_MR2','iter2_MR0','iter2_MR1']
+def get_map(l, t, m, n):
+    key = '_'.join([l, t, str(n), str(m)])
+    return info[key]
 
-
-
-for q in eng_short+xit_short:
-    wrd = q.split('_')[1]
-    print q,len(filter(lambda x: x.split('_')[1]==wrd,qer_list))
-
-def get_feature_dist(ftp):
-    return pickle.load(open('qd_dist/'+ftp+'.dat','r'))
-
-def get_pattern_dist(tok):
-    return pickle.load(open('sq_dist/'+tok+'.dat','r'))
-    
-
-eng_list = filter(lambda x: x[:3]=='eng',doc_list) 
-xit_list = filter(lambda x: x[:3]=='xit',doc_list)
-#doc2idx = {}
-#for i in range(len(eng_list)):
-#    doc2idx[eng_list[i]] = i
-#for i in range(len(xit_list)):
-#    doc2idx[xit_list[i]] = i
-
-def get_answ():
-    answ = {}
-    def qer2wrd(qer):
-        return '_'.join(qer.split('_')[:2])
-    def doc2wav(doc):
-        return doc[4:]
-    #same[wrd]=doc_list
-    same = {}
-    wrd_list = list(set(map(qer2wrd,qer_list)))
-    for wrd in wrd_list:
-        same[wrd] = []
-    for qer in qer_list:
-        same[qer2wrd(qer)].append(qer[:3]+'_'+qd_parse.qer_time[qer][0])
-    for qer in xqer_list:
-        answ[qer] = same[qer2wrd(qer)]
-    return answ
-answ = get_answ()
-def get_lan_list(qer):
-    if qer[:3]=='eng':
-        lan_list = eng_list
-    if qer[:3]=='xit':
-        lan_list = xit_list
-    return lan_list
-def get_answ_list(qer):
-    lan_list = get_lan_list(qer)
-    answ_list = [1 if doc in answ[qer] else 0 for doc in lan_list]
-                
-    return answ_list
-def get_dist():
-    dist = {}
-    for ftp in ftp_list:
-        dist[ftp] = get_feature_dist(ftp)
-    for tok in tok_list:
-        dist[tok] = get_pattern_dist(tok)
-    return dist
-dist = get_dist()
-def get_dist_list(qer,ftpok):
-    lan_list = get_lan_list(qer)
-    dist_list = [- dist[ftpok][qer,doc] for doc in lan_list]
-    return dist_list
-from zrst.util import average_precision_minus1 as ap
-#ap = util.average_precision_minus1
-
-
-def get_map(method, opt, q_list):
-    return np.mean([ap(get_answ_list(q), method(q, opt)) 
-            for q in q_list])
-        
-def avg_mn(q, ln_it_mr):
-    if q[:3]=='xit':
-        m_list = ['50','100','300','500']
-    elif q[:3]=='eng':
-        m_list = ['50','100','300']
-    score = []
+def get_xyz(l, t):
+    x, y, z = [], [], []
+    if l=='eng':    
+        m_list = [3,5,7]
+    else: 
+        m_list = [3,5,7,9]
     for m in m_list:
-        for n in ['3','5','7']:
-            tok = ln_it_mr+'_'+m+'_'+n
-            score.append(get_dist_list(q, tok))
-    return np.sum(np.array(score),axis = 0)
-def avg_it_mr_mn(q,ln):
-    score = []
-    for it_mr in it_mr_list:
-        lnitmr = ln+'_'+it_mr
-        score.append(avg_mn(q, lnitmr))
-    return np.sum(np.array(score),axis = 0)
-
-def avg_all(q,ln):
-    score = []
-    score.append(avg_it_mr_mn(q, ln))
-    score.append(get_dist_list(q,'mfc'))
-    score.append(get_dist_list(q,'mbf'))
-    return np.sum(np.array(score),axis = 0)
-if __name__=='__main__':
-    '''
-    for q in xqer_list:
-        oo = get_answ_list(q)
-        for ftp in ftp_list:
-            xx = get_dist_list(q,ftp)
-            print q, ftp, ap(oo, xx)
-     
-        for tok in tok_list:
-            if tok[:3]!=q[:3]:continue
-            xx = get_dist_list(q,tok)
-            print q, tok, ap(oo, xx)
-    '''
-    eng_tok = filter(lambda x: x[:3]=='eng',tok_list)
-    xit_tok = filter(lambda x: x[:3]=='xit',tok_list)
-    #eng too low...
-    eng_short = eng_short[:5]
-    for tok in eng_tok:
-        print 'eng',tok, get_map(get_dist_list, tok, eng_short)
-    for tok in xit_tok:
-        print 'xit',tok, get_map(get_dist_list, tok, xit_short)
-
-    for ftp in ftp_list:
-        print 'eng',ftp, get_map(get_dist_list, ftp, eng_short) 
-    for ftp in ftp_list:
-        print 'xit',ftp, get_map(get_dist_list, ftp, xit_short) 
-
-    for it_mr in it_mr_list:
-        print 'eng',it_mr, get_map(avg_mn, 'eng_'+it_mr, eng_short) 
-    for it_mr in it_mr_list:
-        print 'xit',it_mr, get_map(avg_mn, 'xit_'+it_mr, xit_short) 
-
-    print 'eng', 'avg_itmr', get_map(avg_it_mr_mn, 'eng', eng_short) 
-    print 'xit', 'avg_itmr', get_map(avg_it_mr_mn, 'xit', xit_short) 
-
-    print 'eng', 'avg_all', get_map(avg_all, 'eng', eng_short) 
-    print 'xit', 'avg_all', get_map(avg_all, 'xit', xit_short) 
+        for n in n_list:
+            x.append(m)
+            y.append(n)
+            z.append(get_map(l,t,m,n))
+    x, y, z = map(lambda f: np.array(f, dtype=np.float32), [x,y,z])
+    return x, y, z
     
-    '''
-    for tok in tok_list:
-        if sys.argv[1] not in tok: continue
-        S = ph_dist.get_dm(tok)
-        dist = {}
-        for q in qer_list:
-            if q[:3]!=tok[:3]: continue
-            print tok, q
-            Q = parse_qer_pattern(q,tok)
-            for d in doc_list:
-                if q[:3]!=d[:3]: continue
-                D = parse_doc_pattern(d,tok)
-                dist[q,d] = warp_pattern(Q,D,S)
-                #print q,d, dist[q,d] 
-        pickle.dump(dist, open('sq_dist/'+tok+'.dat','w'))
+if __name__=='__main__':
+    l = l_list[1]
+    t = t_list[1]
+    if l=='eng':    
+        m_list = [3,5,7]
+    else: 
+        m_list = [3,5,7,9]
+    for n in n_list:
+        for m in m_list:
+            print get_map(l,t, m, n)
+    
+
+    m_min, m_max = 0, 11
+    n_min, n_max = 0, 600 
+    inter = 100
+    
+    #x_list = np.array([m_min]+ m_list+ [m_max])
+    #y_list = np.array([n_min]+ n_list+ [n_max])
+    
+    x_list = np.array(m_list)
+    y_list = np.array(n_list)
+    x, y, z = get_xyz(l, t)
+    z_max = max(z)
+    z_list = z.reshape(x_list.shape[0],y_list.shape[0])
+    
+    xi = np.linspace(m_min, m_max, inter)
+    yi = np.linspace(n_min, n_max, inter)
+    xv, yv = np.meshgrid(xi, yi)
+    
+    print x,y,z
+    print xv.shape, yv.shape
+
+    def inter_2dsp(x,y,z,xv,yv):
+        from scipy.interpolate import RectBivariateSpline
+        rbs = RectBivariateSpline(x_list,y_list,z_list,kx=2,ky=2)#,bbox = [m_min, m_max, n_min, n_max])
+        zv = rbs(xi,yi)
+        print zv.shape
+        return zv
+    def inter_near(x,y,z,xv,yv):
+        from scipy.interpolate import griddata
+        zv = griddata((x, y), z, (xv, yv), method='nearest')
+        return zv
+    def inter_cube(x,y,z,xv,yv):
+        from scipy.interpolate import griddata
+        zv = griddata((x, y), z, (xv, yv), method='cubic')
+        return zv
+    def inter_rbfi(x,y,z,xv,yv):
+        from scipy.interpolate import Rbf
+        rbfi = Rbf(x, y, z)
+        zv = rbfi(xv, yv)
+        return zv
+    
+    def plot_cont():
+        plt.contour
+        return CS 
+ 
+    import matplotlib.pyplot as plt
+    import matplotlib.cm as cm
+    plt.figure()
+    #CS = plt.contourf(xv, yv, inter_near(x,y,z,xv,yv))
+    #CS = plt.contourf(xv, yv, inter_rbfi(x,y,z,xv,yv))
+    #CS = plt.contourf(xv, yv, inter_2dsp(x,y,z,xv,yv))
+    #CS = plt.contourf(xv, yv, inter_cube(x,y,z,xv,yv))
+    #colors = cm.rainbow(np.linspace(0, 1, 10))   
+    plt.scatter(xv, yv, c=inter_2dsp(x,y,z,xv,yv), vmin=0, vmax=z_max)
+    plt.colorbar()
+    plt.scatter(x, y)
+    
+    #plt.clabel(CS, inline=1, fontsize=10)
+    plt.title('Simplest default with labels')
+    #plt.xticks(mp_list, map(str,mp_list), rotation='vertical')
+    #plt.yticks(np_list, map(str,np_list))
+    #plt.imshow(grid_0.T)
+    #plt.gcf().set_size_inches(6, 6)
+    plt.show()
+    
+    ''' 
+    def func(x, y):
+        return x*(1-x)*np.cos(4*np.pi*x) * np.sin(4*np.pi*y**2)**2
+    grid_x, grid_y = np.mgrid[0:1:100j, 0:1:200j]
+
+    points = np.random.rand(1000, 2)
+    values = func(points[:,0], points[:,1])
+
+    from scipy.interpolate import griddata
+    grid_z0 = griddata(points, values, (grid_x, grid_y), method='nearest')
+    grid_z1 = griddata(points, values, (grid_x, grid_y), method='linear')
+    grid_z2 = griddata(points, values, (grid_x, grid_y), method='cubic')
+    import matplotlib.pyplot as plt
+    plt.subplot(221)
+    plt.imshow(func(grid_x, grid_y).T, extent=(0,1,0,1), origin='lower')
+    plt.plot(points[:,0], points[:,1], 'k.', ms=1)
+    plt.title('Original')
+    plt.subplot(222)
+    plt.imshow(grid_z0.T, extent=(0,1,0,1), origin='lower')
+    plt.title('Nearest')
+    plt.subplot(223)
+    plt.imshow(grid_z1.T, extent=(0,1,0,1), origin='lower')
+    plt.title('Linear')
+    plt.subplot(224)
+    plt.imshow(grid_z2.T, extent=(0,1,0,1), origin='lower')
+    plt.title('Cubic')
+    plt.gcf().set_size_inches(6, 6)
+    plt.show()
     '''
